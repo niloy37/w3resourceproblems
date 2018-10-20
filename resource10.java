b6bbb1e5593d55f739897ae17f663ed055d5291b
Niloy Rahman <havocisgod@gmail.com>
2018-10-20 08:15:30 +0600

    Add files via upload

---
+++ resource10.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+
+class solution {
+public static void main (String [] args) {
+
+System.out.println(4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11)));
+
+}
+
+}
